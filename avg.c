f454c354fa0a14e2bc4377d060138acd1956b8e7
charan23KD1A0566 <155797843+charan23KD1A0566@users.noreply.github.com>
2024-02-19 12:10:21 +0530

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,13 @@
+/* c program to calculate of three numbers: */
+#include<stdio.h>
+void main()
+{
+int num1,num2,num3;
+int avg;
+printf("Average of three numbers:\n");
+printf("Enter first number:Enter secound number:Enter third number:");
+scanf("%d%d%d",&num1,&num2,&num3);
+printf("Average of %d and %d and %d is %d",num1,num2,num3,avg);
+getch();
+}
+
